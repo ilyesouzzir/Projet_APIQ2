79fca6a05043746addcf1d2e1b61e255364b4dce
ouzzir <ilyes.ouzzir@condorcet.be>
2024-04-20 17:51:53 +0200

    2eme  commit composite

--- src/DesginPatterns/composite/TestComposite.java
+++ src/DesginPatterns/composite/TestComposite.java
@@ -4,20 +4,20 @@ import java.math.BigDecimal;
 
 public class TestComposite{
     public static void main(String[] args) {
-        // Create instances of Course with different amounts of prizeMoney
-        Course race1 = new Course(1, "Race 1", new BigDecimal("1000.00"));
-        Course race2 = new Course(2, "Race 2", new BigDecimal("2000.00"));
-        Course race3 = new Course(3, "Race 3", new BigDecimal("3000.00"));
+        // Create instances of Championnat with different amounts of prizeMoney
+        Championnat championnat1 = new Championnat(1, "Championnat 1", new BigDecimal("1000.00"));
+        Championnat championnat2 = new Championnat(2, "Championnat 2", new BigDecimal("2000.00"));
+        Championnat championnat3 = new Championnat(3, "Championnat 3", new BigDecimal("3000.00"));
 
         // Create an instance of Championnat
-        Championnat championnat = new Championnat(1, "Championnat 1", new BigDecimal("5000.00"));
+        Championnat superChampionnat = new Championnat(4, "Super Championnat", new BigDecimal("5000.00"));
 
-        // Add elements to championnat
-        championnat.addElement(race1);
-        championnat.addElement(race2);
-        championnat.addElement(race3);
+        // Add elements to superChampionnat
+        superChampionnat.addElement(championnat1);
+        superChampionnat.addElement(championnat2);
+        superChampionnat.addElement(championnat3);
 
         // Calculate and display the total prizeMoney
-        System.out.println("Total Prize Money: " + championnat.totPrizeMoney());
+        System.out.println("Total Prize Money: " + superChampionnat.totPrizeMoney());
     }
 }
